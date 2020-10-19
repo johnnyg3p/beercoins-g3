d8d404184cdaecf9a87eedfacb7b52728eba109f
Johnny Brito <johnny.brito@globalhitss.com.br>
2020-10-19 14:06:05 -0300

    add login

---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { Router } from 'react-router';
+import { Redirect, Route } from 'react-router-dom';
+import { History, Location } from 'history'
+
+import SignIn from './screens/SignIn/SignIn';
+
+const Routes = () => (
+  <h1>teste</h1>
+  // <Router history={hashHistory}>    
+  //     <Route path="/" component={SignIn} />    
+  //     <Route path="/login" component={SignIn} />    
+  //     <Redirect from='*' to='/'/>   
+  // </Router>
+);
+
+export default Routes;
---
+++ src/services/Interfaces/ILoginStatus.ts
@@ -0,0 +1,4 @@
+export default interface ILoginStatus {
+    accessToken: string;
+    status: string;   
+}
