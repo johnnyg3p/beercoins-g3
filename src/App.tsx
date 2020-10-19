6402d9b8ab5ef39a54c66ab07ce1e9b218a559a0
Rodrigo_Santos <rodrigo_r@localhost.localdomain>
2020-10-19 19:03:39 -0300

    [ADD] sideBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SideBar from './sideBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <SideBar moderator={true}/>
+        <h1>BEER COIN</h1>
       </header>
     </div>
   );
