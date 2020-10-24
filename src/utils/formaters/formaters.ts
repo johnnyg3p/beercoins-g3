64604ce228a64472431592524612f657239874a9
Celso Hashimoto <celsoyh@gmail.com>
2020-10-24 18:25:46 -0300

    Adjust currency value on operations modal - Create new formater that converts currency string into an integer number; - Operation deposit model now parse number into currency;

--- src/utils/formaters/formaters.ts
+++ src/utils/formaters/formaters.ts
@@ -2,6 +2,16 @@ export const formatCurrency = (currency: number): string => {
   return currency.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
 };
 
+export const formatCurrencyWithouCurrencyDisplay = (currency: number): string => {
+  const formatedCurrency = currency.toLocaleString("pt-BR", { style: "currency", currency: "BRL"});
+
+  return formatedCurrency.replace('R$ ', '');
+};
+
+export const formatCurrencyIntoInteger = (currency: string): number => {
+  return Number(currency.replace(/\D/g, "")) / 100;
+}
+
 export const formatBankPost = (bankPost: DebitCredit): string => {
   return bankPost === "D" ? "Débito" : "Crédito";
 };
