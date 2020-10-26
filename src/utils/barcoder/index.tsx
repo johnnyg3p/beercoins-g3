70e28d0dcb0bc74d6d536c7f7f23e51cd363c5b6
marcelo <marcelo.nascimento@ab-inbev.com>
2020-10-26 02:23:43 -0300

    Barcoder refactor

--- src/utils/barcoder/index.tsx
+++ src/utils/barcoder/index.tsx
@@ -6,21 +6,24 @@ interface IBarcode {
   amount: string;
 }
 
+interface IBank {
+  [key: string]: string;
+}
+
 const banks = (bank: string): string => {
-  const list = [
-    { code: "001", value: "Banco do Brasil" },
-    { code: "104", value: "Caixa Econômica Federal" },
-    { code: "75", value: "Banco ABN Amro S.A." },
-    { code: "477", value: "Banco Citibank" },
-    { code: "341", value: "Banco Itaú Unibanco" },
-    { code: "422", value: "Banco Safra" },
-    { code: "033", value: "Banco Santander" },
-    { code: "237", value: "Banco Bradesco" },
-    { code: "212", value: "Banco Original" },
-    { code: "748", value: "Banco Cooperativo Sicredi" },
-  ];
-  let selectedBank = list.find((item) => item.code === bank)?.value;
-  return `${bank} - ${selectedBank || "Banco não encontrado"}`;
+  const list: IBank = {
+    "001": "Banco do Brasil",
+    "104": "Caixa Econômica Federal",
+    "75": "Banco ABN Amro S.A.",
+    "477": "Banco Citibank",
+    "341": "Banco Itaú Unibanco",
+    "422": "Banco Safra",
+    "033": "Banco Santander",
+    "237": "Banco Bradesco",
+    "212": "Banco Original",
+    "748": "Banco Cooperativo Sicredi",
+  };
+  return `${bank} - ${list[bank] || "Banco não encontrado"}`;
 };
 
 export const decoder = (barcoder: string): IBarcode => {
